90da3f0f451ab93362680bfcf2c52cce855057e5
Timothy Eng <tim.eng@gmail.com>
2014-03-08 19:53:54 -0500

    Initial commit

---
+++ Vigenere/frmMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Vigenere
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCipherKey = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtCipherKey = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCipherKey
+            // 
+            this.lblCipherKey.AutoSize = true;
+            this.lblCipherKey.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCipherKey.Location = new System.Drawing.Point(12, 13);
+            this.lblCipherKey.Name = "lblCipherKey";
+            this.lblCipherKey.Size = new System.Drawing.Size(64, 13);
+            this.lblCipherKey.TabIndex = 0;
+            this.lblCipherKey.Text = "Cipher Key:";
+            this.lblCipherKey.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(21, 36);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(55, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Message:";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtCipherKey
+            // 
+            this.txtCipherKey.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCipherKey.Location = new System.Drawing.Point(80, 10);
+            this.txtCipherKey.Name = "txtCipherKey";
+            this.txtCipherKey.Size = new System.Drawing.Size(341, 22);
+            this.txtCipherKey.TabIndex = 2;
+            this.txtCipherKey.Text = "REDDIT";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(80, 33);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(341, 125);
+            this.txtMessage.TabIndex = 3;
+            this.txtMessage.Text = "ZEJFOKHTMSRMELCPODWHCGAW";
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(136, 164);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "&Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(217, 164);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "&Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(298, 164);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(80, 193);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(341, 125);
+            this.txtResult.TabIndex = 8;
+            this.txtResult.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtResult_KeyDown);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(33, 196);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(42, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Result:";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(435, 328);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtCipherKey);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblCipherKey);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vigenère cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCipherKey;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtCipherKey;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Vigenere/frmMain.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Problem of the Day: 3/6/14
+ * http://www.problemotd.com/
+ * 
+ * Vigenère cipher
+ * The Vigenère cipher made its rounds in the mid-1550s up until the end of the
+ * American Civil War. It was very easy for soldiers to encode messages and
+ * pass them around to all the allied camps.
+ *
+ * The cipher requires a key and a message. It works like this:
+ *
+ *     Key: REDDIT
+ * Message: TODAYISMYBIRTHDAY
+ *
+ * REDDITREDDITREDDI
+ * TODAYISMYBIRTHDAY
+ * -----------------
+ * KSGDGBJQBEQKKLGDG
+ *
+ * Using a 0 based alphabet (A=0), R is the 17th letter of the alphabet and T
+ * is the 19th letter of the alphabet. (17 + 19) mod 26 = 11 which is where K
+ * resides in the alphabet. Repeat for each key/message letter combination
+ * until done.
+ *
+ * Today's problem of the day is two part. The first part is to implement a
+ * Vigenère cipher in the programming language of your choice. Feel free to
+ * post solutions or links to solutions in the comments.
+ *
+ * The second part is to try and implement something to crack the message below
+ * (the key is 5 or less characters).
+ *
+ * ZEJFOKHTMSRMELCPODWHCGAW (Solution: DAY)
+ *
+ * Good luck!
+ */
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vigenere
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCipherKey.Text = "";
+            txtMessage.Text = "";
+            txtResult.Text = "";
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtCipherKey.Text))
+            {
+                MessageBox.Show("Please enter a cipher key to use.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                MessageBox.Show("Please enter a message to decrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtCipherKey.Text = SanitizeString(txtCipherKey.Text);
+            txtMessage.Text = SanitizeString(txtMessage.Text);
+            Vigenere vigenere = new Vigenere(txtCipherKey.Text, txtMessage.Text);
+            vigenere.Decrypt();
+            txtResult.Text = vigenere.Result;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtCipherKey.Text))
+            {
+                MessageBox.Show("Please enter a cipher key to use.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                MessageBox.Show("Please enter a message to encrypt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtCipherKey.Text = SanitizeString(txtCipherKey.Text);
+            txtMessage.Text = SanitizeString(txtMessage.Text);
+            Vigenere vigenere = new Vigenere(txtCipherKey.Text, txtMessage.Text);
+            vigenere.Encrypt();
+            txtResult.Text = vigenere.Result;
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+                txtMessage.SelectAll();
+        }
+
+        private void txtResult_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+                txtMessage.SelectAll();
+        }
+
+        private string SanitizeString(string message)
+        {
+            StringBuilder trimmedMessage = new StringBuilder();
+
+            message = message.ToUpper().Trim();
+            for (int i = 0; i < message.Length; i++)
+                if (message[i] >= 'A' && message[i] <= 'Z') // remove non-characters
+                    trimmedMessage.Append(message[i]);
+
+            return trimmedMessage.ToString();
+        }
+    }
+
+    public class Vigenere
+    {
+        string message;
+        int keyLength;
+        int messageLength;
+
+        public Vigenere(string key, string message)
+        {
+            this.Key = key;
+            keyLength = key.Length;
+            this.message = message;
+            messageLength = message.Length;
+        }
+
+        public string Result { get; private set; }
+
+        public string Key { get; private set; }
+
+        // http://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher#Algebraic_description
+        public void Decrypt()
+        {
+            StringBuilder cryptoString = new StringBuilder();
+
+            for (int i = 0; i < messageLength; i++)
+            {
+                // Broken down for legibility
+                int keyChar = (int)Key[i % keyLength] - 65;
+                int messageChar = (int)message[i] - 65;
+                int charDifference = messageChar - keyChar;
+                int newCharValue = (charDifference < 0) ? (charDifference + 26) : (charDifference % 26);
+                char newChar = (char)(newCharValue + 65);
+                cryptoString.Append(newChar);
+            }
+
+            Result = cryptoString.ToString();
+        }
+
+        public void Encrypt()
+        {
+            StringBuilder cryptoString = new StringBuilder();
+
+            for (int i = 0; i < messageLength; i++)
+            {
+                // Broken down for legibility
+                int keyChar = (int)Key[i % keyLength] - 65;
+                int messageChar = (int)message[i] - 65;
+                int newCharValue = (keyChar + messageChar) % 26;
+                char newChar = (char)(newCharValue + 65);
+                cryptoString.Append(newChar);
+            }
+
+            Result = cryptoString.ToString();
+        }
+    }
+}
